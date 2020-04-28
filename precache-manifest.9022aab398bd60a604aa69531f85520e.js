53b5ae904ed9993318f8fcd4a009cd51ae842e52
gipark <gipark1993@gmail.com>
2020-04-28 13:41:54 +0900

    Updates

---
+++ precache-manifest.9022aab398bd60a604aa69531f85520e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c899bfd3e9ffcbdaa0d54ff398d27da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de156cecd3db9f1d4466",
+    "url": "/static/css/main.70155342.chunk.css"
+  },
+  {
+    "revision": "e07c9058315706347c14",
+    "url": "/static/js/2.d22755f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d22755f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de156cecd3db9f1d4466",
+    "url": "/static/js/main.e810efd2.chunk.js"
+  },
+  {
+    "revision": "b775e3132e0930477ff9",
+    "url": "/static/js/runtime-main.17a0efde.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.8cfd228fef5e1bd5150e5b549f47f211.js"
+  "/precache-manifest.9022aab398bd60a604aa69531f85520e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
